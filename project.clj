70e751d0f48c59bb780c1cc983da7ef38d0c63d2
Sam Ritchie <sritchie09@gmail.com>
2011-06-26 18:14:48 -0400

    added basic clojure namespace. Updated project.clj with Clojure dependencies.

--- project.clj
+++ project.clj
@@ -1,12 +1,17 @@
-(defproject backtype/dfs-datastores "1.0.3"
+(defproject redd/dfs-datastores "1.0.4"
+  :description "A dramatically simpler and more powerful way to store
+records on a distributed filesystem."
+  :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  
-  :dependencies [
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [jvyaml "1.0.0"]
-                 [com.google.collections/google-collections "1.0"]
-                 ]
-  :dev-dependencies [
-                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                 [com.google.collections/google-collections "1.0"]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [junit/junit "3.8.2"]
-                    ])
+                     [swank-clojure "1.4.0-SNAPSHOT"]
+                     [clojure-source "1.2.0"]
+                     [marginalia "0.5.1"]
+                     [midje "1.2-alpha3"]]
+  :tasks [marginalia.tasks])
